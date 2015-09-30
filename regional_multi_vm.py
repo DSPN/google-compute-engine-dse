b2b86e1be3cc497f08af2316b7bec93066262c5e
benofben <ben.lackey@outlook.com>
2015-09-30 16:37:09 -0600

    Reformat.  Delete old yaml.  Rename new yam.

--- regional_multi_vm.py
+++ regional_multi_vm.py
@@ -1,21 +1,21 @@
 import yaml
 import copy
 
+
 def GenerateConfig(context):
+    # YAML config.
+    config = {'resources': []}
 
-  # YAML config.
-  config = {'resources' : []}
+    # A zonal vm_multiple_instances resource for each zone in the properties list.
+    for zone in context.properties['zones']:
+        newproperties = copy.deepcopy(context.properties)
+        newproperties["zone"] = zone
+        service = {
+            'name': context.env['deployment'] + '-service-' + zone,
+            'type': 'vm_multiple_instances.py',
+            'properties': newproperties
+        }
 
-  # A zonal vm_multiple_instances resource for each zone in the properties list.
-  for zone in context.properties['zones']:
-    newproperties = copy.deepcopy(context.properties)
-    newproperties["zone"] = zone   
-    service = {
-      'name': context.env['deployment'] + '-service-' + zone,
-      'type': 'vm_multiple_instances.py',
-      'properties': newproperties
-    }
-    
-    config['resources'].append(service)
+        config['resources'].append(service)
 
-  return yaml.dump(config)
+    return yaml.dump(config)
--- vm_instance.py
+++ vm_instance.py
@@ -59,144 +59,144 @@ ATTACHED_DISKS = 'ATTACHED_DISKS'
 
 
 def GenerateComputeVM(context):
-  """Generates one VM instance resource."""
-  name = context.env['name']
-  prop = context.properties
-  if SRCIMAGE not in prop:
-    raise common.Error('"%s" is a mandatory property' % SRCIMAGE)
-  boot_disk_type = prop.setdefault(BOOTDISKTYPE, DEFAULT_DISKTYPE)
-  prop[default.DISKTYPE] = boot_disk_type
-  can_ip_fwd = prop.setdefault(CAN_IP_FWD, DEFAULT_IP_FWD)
-  disks = prop.setdefault(default.DISKS, list())
-  # Temporary alternative while multiple disks on creation is not allowed
-  if disks:
-    new_disks = prop.setdefault(default.DISK_RESOURCES, list())
-    disks, prop[DISK_RESOURCES] = GenerateDisks(context, disks, new_disks)
-  machine_type = prop.setdefault(MACHINETYPE, DEFAULT_MACHINETYPE)
-  metadata = prop.setdefault(METADATA, dict())
-  network = prop.setdefault(NETWORK, DEFAULT_NETWORK)
-  named = INSTANCE_NAME in prop
-  provide_boot = prop.setdefault(PROVIDE_BOOT, DEFAULT_PROVIDE_BOOT)
-  tags = prop.setdefault(TAGS, dict([('items', [])]))
-  vm_name = prop[INSTANCE_NAME] if named else common.AutoName(name,
-                                                              default.INSTANCE)
-  zone = prop.setdefault(ZONE, DEFAULT_ZONE)
-  if provide_boot:
-    dev_mode = DEVIMAGE in prop and prop[DEVIMAGE]
-    src_image = common.MakeC2DImageLink(prop[SRCIMAGE], dev_mode)
-    boot_name = common.AutoName(context.env['name'], default.DISK, 'boot')
-    disk_size = (prop[BOOTDISKSIZE] if BOOTDISKSIZE in prop else
-                 DEFAULT_BOOTDISKSIZE)
-    disk_type = common.MakeLocalComputeLink(context, default.DISKTYPE)
-    disks = PrependBootDisk(disks, boot_name, disk_type, disk_size, src_image)
-  machine_type = common.MakeLocalComputeLink(context, default.MACHINETYPE)
-  network = common.MakeGlobalComputeLink(context, default.NETWORK)
-  # To be consistent with Dev console and gcloud, service accounts need to be
-  #  explicitly disabled
-  remove_scopes = prop[NO_SCOPE] if NO_SCOPE in prop else False
-  if remove_scopes and SERVICE_ACCOUNTS in prop:
-    prop.pop(SERVICE_ACCOUNTS)
-  else:  # Make sure there is a default service account
-    prop.setdefault(SERVICE_ACCOUNTS, copy.deepcopy(DEFAULT_SERVICE_ACCOUNT))
+    """Generates one VM instance resource."""
+    name = context.env['name']
+    prop = context.properties
+    if SRCIMAGE not in prop:
+        raise common.Error('"%s" is a mandatory property' % SRCIMAGE)
+    boot_disk_type = prop.setdefault(BOOTDISKTYPE, DEFAULT_DISKTYPE)
+    prop[default.DISKTYPE] = boot_disk_type
+    can_ip_fwd = prop.setdefault(CAN_IP_FWD, DEFAULT_IP_FWD)
+    disks = prop.setdefault(default.DISKS, list())
+    # Temporary alternative while multiple disks on creation is not allowed
+    if disks:
+        new_disks = prop.setdefault(default.DISK_RESOURCES, list())
+        disks, prop[DISK_RESOURCES] = GenerateDisks(context, disks, new_disks)
+    machine_type = prop.setdefault(MACHINETYPE, DEFAULT_MACHINETYPE)
+    metadata = prop.setdefault(METADATA, dict())
+    network = prop.setdefault(NETWORK, DEFAULT_NETWORK)
+    named = INSTANCE_NAME in prop
+    provide_boot = prop.setdefault(PROVIDE_BOOT, DEFAULT_PROVIDE_BOOT)
+    tags = prop.setdefault(TAGS, dict([('items', [])]))
+    vm_name = prop[INSTANCE_NAME] if named else common.AutoName(name,
+                                                                default.INSTANCE)
+    zone = prop.setdefault(ZONE, DEFAULT_ZONE)
+    if provide_boot:
+        dev_mode = DEVIMAGE in prop and prop[DEVIMAGE]
+        src_image = common.MakeC2DImageLink(prop[SRCIMAGE], dev_mode)
+        boot_name = common.AutoName(context.env['name'], default.DISK, 'boot')
+        disk_size = (prop[BOOTDISKSIZE] if BOOTDISKSIZE in prop else
+                     DEFAULT_BOOTDISKSIZE)
+        disk_type = common.MakeLocalComputeLink(context, default.DISKTYPE)
+        disks = PrependBootDisk(disks, boot_name, disk_type, disk_size, src_image)
+    machine_type = common.MakeLocalComputeLink(context, default.MACHINETYPE)
+    network = common.MakeGlobalComputeLink(context, default.NETWORK)
+    # To be consistent with Dev console and gcloud, service accounts need to be
+    #  explicitly disabled
+    remove_scopes = prop[NO_SCOPE] if NO_SCOPE in prop else False
+    if remove_scopes and SERVICE_ACCOUNTS in prop:
+        prop.pop(SERVICE_ACCOUNTS)
+    else:  # Make sure there is a default service account
+        prop.setdefault(SERVICE_ACCOUNTS, copy.deepcopy(DEFAULT_SERVICE_ACCOUNT))
 
-  # pyformat: disable
-  resource = [
-      {
-          'name': vm_name,
-          'type': default.INSTANCE,
-          'properties': {
-              'zone': zone,
-              'machineType': machine_type,
-              'canIpForward': can_ip_fwd,
-              'disks': disks,
-              'networkInterfaces': [{
-                  'network': network,
-                  'accessConfigs': [{'name': default.EXTERNAL,
-                                     'type': default.ONE_NAT}]
-              }],
-              'tags': tags,
-              'metadata': metadata,
-          }
-      }
-  ]
-  # pyformat: enable
+    # pyformat: disable
+    resource = [
+        {
+            'name': vm_name,
+            'type': default.INSTANCE,
+            'properties': {
+                'zone': zone,
+                'machineType': machine_type,
+                'canIpForward': can_ip_fwd,
+                'disks': disks,
+                'networkInterfaces': [{
+                    'network': network,
+                    'accessConfigs': [{'name': default.EXTERNAL,
+                                       'type': default.ONE_NAT}]
+                }],
+                'tags': tags,
+                'metadata': metadata,
+            }
+        }
+    ]
+    # pyformat: enable
 
-  # Pass through any additional property to the VM
-  if SERVICE_ACCOUNTS in prop:
-    resource[0]['properties'].update({SERVICE_ACCOUNTS: prop[SERVICE_ACCOUNTS]})
-  return resource
+    # Pass through any additional property to the VM
+    if SERVICE_ACCOUNTS in prop:
+        resource[0]['properties'].update({SERVICE_ACCOUNTS: prop[SERVICE_ACCOUNTS]})
+    return resource
 
 
 def PrependBootDisk(disk_list, name, disk_type, disk_size, src_image):
-  """Appends the boot disk."""
-  # Request boot disk creation (mark for autodelete)
-  boot_disk = [{
-      'autoDelete': True,
-      'boot': True,
-      'deviceName': name,
-      'initializeParams': {
-          'diskType': disk_type,
-          'diskSizeGb': disk_size,
-          'sourceImage': src_image
-      },
-      'type': DEFAULT_PERSISTENT,
-  }]
-  return boot_disk + disk_list
+    """Appends the boot disk."""
+    # Request boot disk creation (mark for autodelete)
+    boot_disk = [{
+        'autoDelete': True,
+        'boot': True,
+        'deviceName': name,
+        'initializeParams': {
+            'diskType': disk_type,
+            'diskSizeGb': disk_size,
+            'sourceImage': src_image
+        },
+        'type': DEFAULT_PERSISTENT,
+    }]
+    return boot_disk + disk_list
 
 
 def GenerateDisks(context, disk_list, new_disks):
-  """Generates as many disks as passed in the disk_list."""
-  project = context.env[default.PROJECT]
-  prop = context.properties
-  zone = prop.setdefault(ZONE, DEFAULT_ZONE)
-  sourced_disks = []
-  disk_names = []
-  for disk in disk_list:
-    d_name = (disk[default.DEVICE_NAME] if default.DISK_NAME not in disk else
-              disk[default.DISK_NAME])
-    if default.DISK_SOURCE in disk:  # Existing disk, expect disk api link
-      source = disk[default.DISK_SOURCE]
-    else:  # The disks should be create separately
-      if default.DEVICE_NAME not in disk and default.DISK_NAME not in disk:
-        raise common.Error('deviceName or diskName is needed for each disk in '
-                           'this module implemention of multiple disks per vm.')
-      disk_init = disk.setdefault(default.INITIALIZEP, dict())
-      disk_size = disk_init.setdefault(default.DISK_SIZE, DEFAULT_DATADISKSIZE)
-      passed_disk_type = disk_init.setdefault(default.TYPE, DEFAULT_DISKTYPE)
-      disk_type = common.LocalComputeLink(
-          project, zone, 'diskTypes', passed_disk_type)
-      new_disks.append({
-          'name': d_name,
-          'type': default.DISK,
-          'properties': {
-              'type': disk_type,
-              'sizeGb': disk_size,
-              'zone': zone
-          }
-      })  # pyformat: disable
-      disk_names.append(d_name)
-      source = common.Ref(d_name)
-    sourced_disks.append({
-        'deviceName': d_name,
-        'autoDelete': True,
-        'boot': False,
-        'source': source,
-        'type': DEFAULT_PERSISTENT,
-    })
+    """Generates as many disks as passed in the disk_list."""
+    project = context.env[default.PROJECT]
+    prop = context.properties
+    zone = prop.setdefault(ZONE, DEFAULT_ZONE)
+    sourced_disks = []
+    disk_names = []
+    for disk in disk_list:
+        d_name = (disk[default.DEVICE_NAME] if default.DISK_NAME not in disk else
+                  disk[default.DISK_NAME])
+        if default.DISK_SOURCE in disk:  # Existing disk, expect disk api link
+            source = disk[default.DISK_SOURCE]
+        else:  # The disks should be create separately
+            if default.DEVICE_NAME not in disk and default.DISK_NAME not in disk:
+                raise common.Error('deviceName or diskName is needed for each disk in '
+                                   'this module implemention of multiple disks per vm.')
+            disk_init = disk.setdefault(default.INITIALIZEP, dict())
+            disk_size = disk_init.setdefault(default.DISK_SIZE, DEFAULT_DATADISKSIZE)
+            passed_disk_type = disk_init.setdefault(default.TYPE, DEFAULT_DISKTYPE)
+            disk_type = common.LocalComputeLink(
+                project, zone, 'diskTypes', passed_disk_type)
+            new_disks.append({
+                'name': d_name,
+                'type': default.DISK,
+                'properties': {
+                    'type': disk_type,
+                    'sizeGb': disk_size,
+                    'zone': zone
+                }
+            })  # pyformat: disable
+            disk_names.append(d_name)
+            source = common.Ref(d_name)
+        sourced_disks.append({
+            'deviceName': d_name,
+            'autoDelete': True,
+            'boot': False,
+            'source': source,
+            'type': DEFAULT_PERSISTENT,
+        })
 
-  items = prop[METADATA].setdefault('items', list())
-  items.append({'key': ATTACHED_DISKS, 'value': ','.join(disk_names)})
-  return sourced_disks, new_disks
+    items = prop[METADATA].setdefault('items', list())
+    items.append({'key': ATTACHED_DISKS, 'value': ','.join(disk_names)})
+    return sourced_disks, new_disks
 
 
 def GenerateResourceList(context):
-  """Returns list of resources generated by this module."""
-  resources = GenerateComputeVM(context)
-  resources += common.AddDiskResourcesIfNeeded(context)
-  return resources
+    """Returns list of resources generated by this module."""
+    resources = GenerateComputeVM(context)
+    resources += common.AddDiskResourcesIfNeeded(context)
+    return resources
 
 
 @common.FormatErrorsDec
 def GenerateConfig(context):
-  """Generates YAML resource configuration."""
-  return common.MakeResource(GenerateResourceList(context))
+    """Generates YAML resource configuration."""
+    return common.MakeResource(GenerateResourceList(context))
