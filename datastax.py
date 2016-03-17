6659f3c9c8833856b22f6dabcc5d091373b0ccfc
benofben <ben.lackey@outlook.com>
2016-03-17 11:23:10 -0700

    use 14.04 lts

--- datastax.py
+++ datastax.py
@@ -42,7 +42,7 @@ def GenerateConfig(context):
         'name': 'clusters-' + context.env['name'],
         'type': 'regional_multi_vm.py',
         'properties': {
-            'sourceImage': 'https://www.googleapis.com/compute/v1/projects/ubuntu-os-cloud/global/images/ubuntu-1510-wily-v20160310',
+            'sourceImage': 'https://www.googleapis.com/compute/v1/projects/ubuntu-os-cloud/global/images/ubuntu-1404-trusty-v20160314',
             'zones': context.properties['zones'],
             'machineType': context.properties['machineType'],
             'network': 'default',
@@ -119,7 +119,7 @@ def GenerateConfig(context):
         'name': context.env['deployment'] + '-opscenter-' + context.env['name'],
         'type': 'vm_instance.py',
         'properties': {
-            'sourceImage': 'https://www.googleapis.com/compute/v1/projects/ubuntu-os-cloud/global/images/ubuntu-1510-wily-v20160310',
+            'sourceImage': 'https://www.googleapis.com/compute/v1/projects/ubuntu-os-cloud/global/images/ubuntu-1404-trusty-v20160314',
             'zone': context.properties['opsCenterZone'],
             'machineType': context.properties['machineType'],
             'network': 'default',
