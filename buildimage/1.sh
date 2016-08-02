f5b6669a9c4ce5a1e0bdb9dff3e093bb2b83e306
benofben <ben.lackey@outlook.com>
2016-08-01 19:03:57 -0700

    using var

--- buildimage/1.sh
+++ buildimage/1.sh
@@ -4,7 +4,7 @@ INSTANCE=instance-1
 
 # Create a VM using one of the supported operating systems.
 # Create a new instance with --scopes "storage-rw"
-gcloud compute --project "datastax-dev" instances create "instance-1" --zone "us-central1-f" --machine-type "n1-standard-8" --network "default" --maintenance-policy "MIGRATE" --scopes default="https://www.googleapis.com/auth/cloud-platform" --image "https://www.googleapis.com/compute/v1/projects/ubuntu-os-cloud/global/images/ubuntu-1404-trusty-v20160314" --boot-disk-size "200" --boot-disk-type "pd-standard" --boot-disk-device-name "instance-1" --scopes "storage-rw"
+gcloud compute --project "datastax-dev" instances create ${INSTANCE} --zone "us-central1-f" --machine-type "n1-standard-8" --network "default" --maintenance-policy "MIGRATE" --scopes default="https://www.googleapis.com/auth/cloud-platform" --image "https://www.googleapis.com/compute/v1/projects/ubuntu-os-cloud/global/images/ubuntu-1404-trusty-v20160314" --boot-disk-size "200" --boot-disk-type "pd-standard" --boot-disk-device-name ${INSTANCE} --scopes "storage-rw"
 
 # Verify that instance exists
 gcloud compute instances describe ${INSTANCE}
