bfb46138b5a4b1208ded47d56a97da5baf4a7974
benofben <ben.lackey@outlook.com>
2016-08-08 09:49:21 -0700

    update image to latest

--- buildimage/1.sh
+++ buildimage/1.sh
@@ -2,9 +2,13 @@
 
 INSTANCE=instance-1
 
+# You're going to want to ensure this image is built with the latest 14.04.  To check what that is, run:
+gcloud compute images list
+IMAGE_NAME=ubuntu-1404-trusty-v20160627
+
 # Create a VM using one of the supported operating systems.
 # Create a new instance with --scopes "storage-rw"
-gcloud compute --project "datastax-dev" instances create ${INSTANCE} --zone "us-central1-f" --machine-type "n1-standard-8" --network "default" --maintenance-policy "MIGRATE" --scopes default="https://www.googleapis.com/auth/cloud-platform" --image "https://www.googleapis.com/compute/v1/projects/ubuntu-os-cloud/global/images/ubuntu-1404-trusty-v20160314" --boot-disk-size "200" --boot-disk-type "pd-standard" --boot-disk-device-name ${INSTANCE} --scopes "storage-rw"
+gcloud compute --project "datastax-dev" instances create ${INSTANCE} --zone "us-central1-f" --machine-type "n1-standard-8" --network "default" --maintenance-policy "MIGRATE" --scopes default="https://www.googleapis.com/auth/cloud-platform" --image "https://www.googleapis.com/compute/v1/projects/ubuntu-os-cloud/global/images/${IMAGE_NAME}" --boot-disk-size "200" --boot-disk-type "pd-standard" --boot-disk-device-name ${INSTANCE} --scopes "storage-rw"
 
 # Verify that instance exists
 gcloud compute instances describe ${INSTANCE}
