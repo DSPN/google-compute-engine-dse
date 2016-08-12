b2fc9960d569fea3e18284d24fed6b7c851fa3bf
benofben <ben.lackey@outlook.com>
2016-08-12 16:03:49 -0700

    format and reorder cleanup

--- buildimage/1.sh
+++ buildimage/1.sh
@@ -6,8 +6,7 @@ INSTANCE=instance-2
 gcloud compute images list
 IMAGE_NAME=ubuntu-1404-trusty-v20160627
 
-# Create a VM using one of the supported operating systems.
-# Create a new instance with --scopes "storage-rw"
+# create an instance with that image
 gcloud compute --project "datastax-dev" instances create ${INSTANCE} --zone "us-central1-f" --machine-type "n1-standard-8" --network "default" --maintenance-policy "MIGRATE" --scopes default="https://www.googleapis.com/auth/cloud-platform" --image "https://www.googleapis.com/compute/v1/projects/ubuntu-os-cloud/global/images/${IMAGE_NAME}" --boot-disk-size "20" --boot-disk-type "pd-standard" --boot-disk-device-name ${INSTANCE} --scopes "storage-rw"
 
 # Verify that instance exists
--- buildimage/2.sh
+++ buildimage/2.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# SSH into the instance:
+# SSH into the instance
 INSTANCE=instance-2
 gcloud compute ssh ${INSTANCE}
 
@@ -12,24 +12,27 @@ apt-get -y install software-properties-common
 add-apt-repository -y ppa:webupd8team/java
 apt-get update
 
-# If you're going through all this trouble, you probably want to check you're using the latest packages
-# I should really try specifying version here...
-apt-cache policy oracle-java8-installer
-
 echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
 echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
-apt-get -y install oracle-java8-installer
+
+# If you're going through all this trouble, you probably want to check you're using the latest packages
+apt-cache policy oracle-java8-installer
+java_version=123
+
+# I should really try specifying version here...
+apt-get -y install oracle-java8-installer=$java_version
 
 ### Download, but do not install DataStax Enterprise
 echo "deb http://datastax%40google.com:8GdeeVT2s7zi@debian.datastax.com/enterprise stable main" | sudo tee -a /etc/apt/sources.list.d/datastax.sources.list
 curl -L http://debian.datastax.com/debian/repo_key | sudo apt-key add -
 apt-get -y update
-dse_version=5.0.1-1
-opscenter_version=6.0.2
 
 # If you're going through all this trouble, you probably want to check you're using the latest packages
 apt-cache policy dse-full
+dse_version=5.0.1-1
+
 apt-cache policy opscenter
+opscenter_version=6.0.2
 
 # Download (but do not install) the DataStax packages
 apt-get -y -d install dse-full=$dse_version dse=$dse_version dse-hive=$dse_version dse-pig=$dse_version dse-demos=$dse_version dse-libsolr=$dse_version dse-libtomcat=$dse_version dse-libsqoop=$dse_version dse-liblog4j=$dse_version dse-libmahout=$dse_version dse-libhadoop-native=$dse_version dse-libcassandra=$dse_version dse-libhive=$dse_version dse-libpig=$dse_version dse-libhadoop=$dse_version dse-libspark=$dse_version
