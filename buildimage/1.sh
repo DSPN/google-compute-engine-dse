4a2d0f1ec0954a9bf958904e72d35f415617e90a
benofben <ben.lackey@outlook.com>
2016-08-12 15:51:36 -0700

    drop disk size to 20gb

--- buildimage/1.sh
+++ buildimage/1.sh
@@ -8,7 +8,7 @@ IMAGE_NAME=ubuntu-1404-trusty-v20160627
 
 # Create a VM using one of the supported operating systems.
 # Create a new instance with --scopes "storage-rw"
-gcloud compute --project "datastax-dev" instances create ${INSTANCE} --zone "us-central1-f" --machine-type "n1-standard-8" --network "default" --maintenance-policy "MIGRATE" --scopes default="https://www.googleapis.com/auth/cloud-platform" --image "https://www.googleapis.com/compute/v1/projects/ubuntu-os-cloud/global/images/${IMAGE_NAME}" --boot-disk-size "200" --boot-disk-type "pd-standard" --boot-disk-device-name ${INSTANCE} --scopes "storage-rw"
+gcloud compute --project "datastax-dev" instances create ${INSTANCE} --zone "us-central1-f" --machine-type "n1-standard-8" --network "default" --maintenance-policy "MIGRATE" --scopes default="https://www.googleapis.com/auth/cloud-platform" --image "https://www.googleapis.com/compute/v1/projects/ubuntu-os-cloud/global/images/${IMAGE_NAME}" --boot-disk-size "20" --boot-disk-type "pd-standard" --boot-disk-device-name ${INSTANCE} --scopes "storage-rw"
 
 # Verify that instance exists
 gcloud compute instances describe ${INSTANCE}
