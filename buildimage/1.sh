f54c052186e379921a1be24eb7252c3df935de29
benofben <ben.lackey@outlook.com>
2016-08-12 18:02:05 -0700

    use 20gb boot

--- buildimage/1.sh
+++ buildimage/1.sh
@@ -7,8 +7,8 @@ gcloud compute images list
 IMAGE_NAME=ubuntu-1404-trusty-v20160809a
 
 # create an instance with that image
-# There's some debate about optimal disk size.  We're going with 10GB for now.
-gcloud compute --project "datastax-dev" instances create ${INSTANCE} --zone "us-central1-f" --machine-type "n1-standard-8" --network "default" --maintenance-policy "MIGRATE" --scopes default="https://www.googleapis.com/auth/cloud-platform" --image "https://www.googleapis.com/compute/v1/projects/ubuntu-os-cloud/global/images/${IMAGE_NAME}" --boot-disk-size "10" --boot-disk-type "pd-standard" --boot-disk-device-name ${INSTANCE} --scopes "storage-rw"
+# There's some debate about boot optimal disk size.  We're going with 20GB for now.
+gcloud compute --project "datastax-dev" instances create ${INSTANCE} --zone "us-central1-f" --machine-type "n1-standard-8" --network "default" --maintenance-policy "MIGRATE" --scopes default="https://www.googleapis.com/auth/cloud-platform" --image "https://www.googleapis.com/compute/v1/projects/ubuntu-os-cloud/global/images/${IMAGE_NAME}" --boot-disk-size "20" --boot-disk-type "pd-standard" --boot-disk-device-name ${INSTANCE} --scopes "storage-rw"
 
 # Verify that instance exists
 gcloud compute instances describe ${INSTANCE}
