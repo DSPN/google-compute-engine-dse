bc1e2b9912bb919051f69434da450c4cf750167f
benofben <ben.lackey@outlook.com>
2016-08-12 17:59:42 -0700

    latest ubuntu image

--- buildimage/1.sh
+++ buildimage/1.sh
@@ -4,7 +4,7 @@ INSTANCE=instance-2
 
 # You're going to want to ensure this image is built with the latest 14.04.  To check what that is, run:
 gcloud compute images list
-IMAGE_NAME=ubuntu-1404-trusty-v20160627
+IMAGE_NAME=ubuntu-1404-trusty-v20160809a
 
 # create an instance with that image
 # There's some debate about optimal disk size.  We're going with 10GB for now.
