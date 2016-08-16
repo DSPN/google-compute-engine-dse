254c27a269185a923e04c67906bf628fd4186bd0
benofben <ben.lackey@outlook.com>
2016-08-15 17:49:37 -0700

    rev to -3

--- datastax.py
+++ datastax.py
@@ -79,9 +79,9 @@ def GenerateConfig(context):
 
         apt-get -y install unzip
 
-        wget https://github.com/DSPN/install-datastax-ubuntu/archive/5.0.1-2.zip
-        unzip 5.0.1-2.zip
-        cd install-datastax-ubuntu-5.0.1-2/bin
+        wget https://github.com/DSPN/install-datastax-ubuntu/archive/5.0.1-3.zip
+        unzip 5.0.1-3.zip
+        cd install-datastax-ubuntu-5.0.1-3/bin
 
         cloud_type="gce"
         seed_nodes_dns_names=''' + seed_nodes_dns_names + '''
@@ -137,9 +137,9 @@ def GenerateConfig(context):
 
       apt-get -y install unzip
 
-      wget https://github.com/DSPN/install-datastax-ubuntu/archive/5.0.1-2.zip
-      unzip 5.0.1-2.zip
-      cd install-datastax-ubuntu-5.0.1-2/bin
+      wget https://github.com/DSPN/install-datastax-ubuntu/archive/5.0.1-3.zip
+      unzip 5.0.1-3.zip
+      cd install-datastax-ubuntu-5.0.1-3/bin
 
       cloud_type="gce"
       seed_nodes_dns_names=''' + seed_nodes_dns_names + '''
