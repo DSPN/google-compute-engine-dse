cd8c0197e15442277c1b9dafc440d3cccfe4c2ae
Gilbert Lau <gmflau@gmail.com>
2018-04-27 09:59:40 -0700

    Using latest GCE stock Ubuntu image

--- buildimage/build_image.sh
+++ buildimage/build_image.sh
@@ -2,8 +2,8 @@
 
 python image_creator.py \
 --project datastax-public \
---disk instance-ubuntu-1404-trusty-v20180110 \
---name datastax-enterprise-ubuntu-1404-trusty-v20180110 \
---description 'DataStax Enterprise Ubuntu 14.04 (2018/01/10) Image' \
+--disk instance-ubuntu-1404-trusty-v20180308 \
+--name datastax-enterprise-ubuntu-1404-trusty-v20180326 \
+--description 'DataStax Enterprise Ubuntu 14.04 (2018/03/26) Image' \
 --destination-project datastax-public \
 --license datastax-public/datastax-enterprise
